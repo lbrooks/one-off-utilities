0777505cb5501118a6c7379d99332f572c0fa8dc
Lawrence Brooks <84.lbrooks@gmail.com>
2017-08-19 10:26:36 -0400

    duplicate scripts?

---
+++ group 2.js
@@ -0,0 +1,25 @@
+var glob = require('glob'),
+  path = require('path'),
+  fs = require('fs');
+
+glob('./*.*', function(err, files) {
+  var processed = 0;
+  files.forEach(function(file) {
+    var fileObj = path.parse(file);
+
+    var regEx_result = fileObj.name.match(/S([0-9]+)/i);
+    var se = regEx_result[1].toUpperCase();
+
+    var seasonFolder = 'Season ' + se;
+    if (!fs.existsSync('./' + seasonFolder)){
+        fs.mkdirSync(seasonFolder);
+    }    
+    if(se){
+      console.log('Move: ' + fileObj.base + '\t -> \t' + './' + seasonFolder + '/');
+
+      fs.renameSync(file, './' + seasonFolder + '/' + fileObj.base);
+      processed++;
+    }
+  });
+  console.log(processed + ' files processed');
+});
